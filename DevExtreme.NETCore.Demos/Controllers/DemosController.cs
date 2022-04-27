e20d47a127acfed3dab57ffec454968d3095ba7d
bocbambooairways <bocbambooairways@gmail.com>
2022-04-27 16:16:50 +0700

    Devexpress

---
+++ DevExtreme.NETCore.Demos/Controllers/DemosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevExtreme.NETCore.Demos.Controllers
+{
+    public class DemosController : Controller
+    {
+        public ActionResult Overview()
+        {
+            return View();
+        }
+    }
+}
---
+++ DevExtreme.NETCore.Demos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DevExtreme.NETCore.Demos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
